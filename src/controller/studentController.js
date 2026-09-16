3305fbab7bab292b304be8516dc9ab6ea7a94996
Sergei Rakhman <sergeirakhman@outlook.com>
2026-09-16 06:47:56 +0300

    added routes, implemented controller's functions

--- src/controller/studentController.js
+++ src/controller/studentController.js
@@ -1,4 +1,5 @@
 import * as studentService from '../service/studentService.js'
+import {err_404} from "../errors/err.js";
 
 export const addStudent = async (req, res) => {
     const success = await studentService.addStudent(req.body);
@@ -9,17 +10,41 @@ export const addStudent = async (req, res) => {
 export const findStudent = async (req, res) => {
     const student = await studentService.findStudent(req.params.id);
 
-    student ? res.json(student) : res.status(404).send({
-        "timestamp": new Date().toISOString(),
-        "status": 404,
-        "error": "Not Found",
-        "message": `Student with id ${req.params.id} not found`,
-        "path": req.params
-    });
+    student ? res.json(student) : res.status(404).send(err_404(req.params.id, req.params));
 };
 
-export const removeStudent = async (req, res) => {};
+export const removeStudent = async (req, res) => {
+    const student = await studentService.removeStudent(req.params.id);
 
-export const updateStudent = async (req, res) => {};
+    student ? res.json(student) : res.status(404).send(err_404(req.params.id, req.params));
+};
+
+export const updateStudent = async (req, res) => {
+    const student = await studentService.updateStudent(req.params.id, req.body);
+
+    student ? res.json(student) : res.status(404).send(err_404(req.params.id, req.params));
+};
+
+export const addScore = async (req, res) => {
+    const success = await studentService.addScore(req.body);
+
+    success ? res.status(204).send() : res.status(404).send(err_404(req.params.id, req.params));
+};
+
+export  const findStudentsByName = async (req, res) => {
+    const students = await studentService.findStudentsByName(req.params.name);
+    res.json(students);
+};
+
+export const countStudentsByNames = async (req, res) => {
+    const names = [].concat(req.params.names || []);
+    const counts = await studentService.countStudentsByNames(names);
+    res.json(counts);
+};
+
+export const findStudentsByMinScore = async (req, res) => {
+    const students = await studentService.findStudentsByMinScore(req.params.exam, req.params.minScore);
+    res.json(students);
+};
 
 
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,9 +1,23 @@
 import {Router} from "express";
-import {addStudent, findStudent} from "../controller/studentController";
+import {
+    addScore,
+    addStudent, countStudentsByNames,
+    findStudent,
+    findStudentsByMinScore,
+    findStudentsByName,
+    updateStudent
+} from "../controller/studentController";
+import {removeStudent} from "../service/studentService.js";
 
 const router = Router();
 
 router.post("/student", addStudent);
-router.put("/student/:id", findStudent);
+router.get("/student/:id", findStudent);
+router.delete("/student/:id", removeStudent);
+router.patch("/student/:id", updateStudent);
+router.post("/score/student/:id", addScore);
+router.get("/students/name/:name", findStudentsByName);
+router.get("//quantity/students", countStudentsByNames);
+router.get("/students/exam/:exam/minscore/:minScore", findStudentsByMinScore);
 
 export default router;
